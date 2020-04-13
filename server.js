2df4fed3218096f2a030bd255dc66963e4448d54
Kerim <kutukmu@gmail.com>
2020-04-13 11:50:03 -0500

    home,login,register,createPost pages creation

--- server.js
+++ server.js
@@ -10,8 +10,22 @@ mongoose.connect("mongodb://localhost:27017/loggerApp", { useUnifiedTopology: tr
 app.set("view engine", "ejs")
 
 
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get("/article", (req, res) => {
+    res.render("article")
+})
 
 
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
 
 app.listen(3000, () => {
     console.log("Server Has Started on Port 3000")
