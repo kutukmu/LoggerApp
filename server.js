4d3efe55f25111cfa5607968fcbd8c65276a3fca
Kerim <kutukmu@gmail.com>
2020-04-13 16:33:25 -0500

    authentication start

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const passportlocalMongoose = require("passport-local-mongoose");
+const methodOverride = require("method-override")
 const app = express();
 
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: true }))
-mongoose.connect("mongodb://localhost:27017/loggerApp", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.sessio());
+
+
+
 
 app.set("view engine", "ejs")
 
@@ -18,8 +35,21 @@ const postSchema = mongoose.Schema({
 
 const Post = new mongoose.model("post", postSchema);
 
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String,
 
+})
+userSchema.plugin(passportlocalMongoose)
 
+const User = new mongoose.model("user", userSchema)
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+mongoose.connect("mongodb://localhost:27017/loggerApp", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.set("useCreateIndex", true)
 app.get("/", (req, res) => {
     Post.find({}, (err, result) => {
         if (!err) {
@@ -49,17 +79,60 @@ app.post("/article", (req, res) => {
 })
 
 app.get("/article/:id", (req, res) => {
-    const title = req.params.id;
-    Post.findOne({ title: title }, (err, result) => {
+    const id = req.params.id;
+
+    Post.findOne({ _id: id }, (err, result) => {
         if (!err) {
             if (result) {
                 res.render("post", { post: result })
+
             }
         }
     })
 
 
 })
+
+app.put("/article/:id", (req, res) => {
+    const id = req.params.id;
+
+    Post.findOneAndUpdate({ _id: id }, {
+        title: req.body.title,
+        url: req.body.url,
+        content: req.body.content
+    }, { overwrite: true }, (err, result) => {
+        if (!err) {
+            res.redirect(`/article/${id}`)
+
+        }
+    })
+})
+
+
+app.delete("/article/:id", (req, res) => {
+    const id = req.params.id;
+
+    Post.findOneAndRemove({ _id: id }, (err, result) => {
+        if (!err) {
+            res.redirect(`/`)
+
+        }
+    })
+})
+
+
+app.get("/article/:id/edit", (req, res) => {
+    const id = req.params.id;
+    Post.findOne({ _id: id }, (err, result) => {
+        if (!err) {
+            if (result) {
+                res.render("edit", { post: result })
+            }
+        }
+    })
+
+})
+
 
 
 
